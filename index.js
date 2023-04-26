61b164e239174aeb81a9a14449b9fd6d8029c7e9
RealCoderUz <saidqodirxonuz@gmail.com>
2023-04-26 17:06:37 +0500

    ready

---
+++ index.js
@@ -0,0 +1,40 @@
+const { HttpServer, ChildServer } = require("lib");
+
+// const server = new HttpServer({ port: 8080 });
+// server.get("/", (request, response) => {
+//   response.end("Hello");
+// });
+// server.get("/ping", (request, response) => {
+//   console.log(request.method);
+//   console.log(request.url);
+
+//   response.setHeader("Content-Type", "application/json");
+//   response.end('{"message": "pong"}');
+// });
+// server.post("/", (request, response) => {
+//   response.end("Foo");
+// });
+// server.listen(() => {
+//   console.log("server 8000-portda ishlayapti");
+// });
+
+// const server2 = new ChildServer(8000);
+
+const server2 = new HttpServer({ port: 8080 });
+server2.get("/", (request, response) => {
+  response.end("Hello");
+});
+server2.get("/ping", (request, response) => {
+  console.log(request.method);
+  console.log(request.url);
+
+  response.setHeader("Content-Type", "application/json");
+  response.end('{"message": "pong"}');
+});
+server2.post("/", (request, response) => {
+  response.end("Foo");
+});
+
+server2.listen(() => {
+  console.log("server2 3002=portda ishlayapti");
+});
